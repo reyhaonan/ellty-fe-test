b58c3191e840e8894ea27bde495458d8b395f9a1
reyhaonan <reyhaonan2003@gmail.com>
2025-11-25 11:39:24 +0700

    fix: button hover state

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react"
 const Button = ({ children, className, ...props }: DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) => {
     return (
         <button
-            className={`bg-primary rounded-sm py-3 px-4 w-full active:bg-primary-active transition-colors cursor-pointer text-sm ${className}`}
+            className={`bg-primary rounded-sm py-3 px-4 w-full hover:bg-primary-hover active:bg-primary transition-colors cursor-pointer ${className}`}
             {...props}
         >
             {children}
